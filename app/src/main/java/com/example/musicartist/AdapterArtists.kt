6ab712635f9615850a2e6541b7d0e355ebbee2f0
aleksandrmilov <smit97rus@mail.ru>
2024-05-22 01:49:28 +0600

    PopularArtist

--- app/src/main/java/com/example/musicartist/AdapterArtists.kt
+++ app/src/main/java/com/example/musicartist/AdapterArtists.kt
@@ -1,52 +1,58 @@
 package com.example.musicartist
 
+import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.musicartist.databinding.PopularArtistItemBinding
 import io.github.jan.supabase.storage.storage
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import java.io.ByteArrayInputStream
 import java.io.InputStream
 
-class AdapterArtists (private val artistsList: List<DataArtist>, private val coroutineScope: CoroutineScope):RecyclerView.Adapter<AdapterArtists.ViewHolder>(){
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): AdapterArtists.ViewHolder {
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.popular_artist_item, parent, false)
-        return ViewHolder(v, coroutineScope)
+class AdapterArtists (private val artistList: List<DataArtist>, private val coroutineScope: CoroutineScope): RecyclerView.Adapter<AdapterArtists.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val itemBinding = PopularArtistItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return ViewHolder(itemBinding, coroutineScope)
     }
 
-    override fun onBindViewHolder(holder: AdapterArtists.ViewHolder, position: Int) {
-        val artists = artistsList[position]
-        holder.bind(artists)
+    override fun getItemCount(): Int = artistList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val artist: DataArtist = artistList[position]
+        holder.bind(artist)
     }
 
-    override fun getItemCount(): Int {
-        return artistsList.size
-    }
+    class ViewHolder(private val itemBinding: PopularArtistItemBinding, private val coroutineScope: CoroutineScope): RecyclerView.ViewHolder(itemBinding.root) {
+        fun bind(artist: DataArtist) { with(itemBinding)
+        {
 
-    class ViewHolder(private val itemView: View, private val coroutineScope: CoroutineScope):RecyclerView.ViewHolder(itemView) {
-    val artistText: TextView = itemView.findViewById(R.id.artistText)
-        val artistView: ImageView = itemView.findViewById(R.id.artistView)
-
-        fun bind(artist: DataArtist) {
             val supabase = SupaBaseClient().ClientSB
             artistText.text = artist.Имя
-            val photo = artist.Аватар
             val bucket = supabase.storage["Performers"]
+
             coroutineScope.launch {
-                val bytes = bucket.downloadPublic(photo.toString())
-                val is1: InputStream = ByteArrayInputStream(bytes)
-                val bmp: Bitmap = BitmapFactory.decodeStream(is1)
-                val dr = BitmapDrawable(itemView.context.resources, bmp)
-                artistView.background = dr
+                try {
+                    val bytes = bucket.downloadPublic(artist.Аватар!!)
+                    val is1: InputStream = ByteArrayInputStream(bytes)
+                    val bmp: Bitmap = BitmapFactory.decodeStream(is1)
+                    val dr = BitmapDrawable(itemView.context.resources, bmp)
+                    artistView.setImageDrawable(dr)
+                }
+                catch (e: Throwable) {
+                    Log.e("AdapterArtists!!!", e.toString())
+                }
+
             }
         }
+        }
     }
-
 }
--- app/src/main/java/com/example/musicartist/Home.kt
+++ app/src/main/java/com/example/musicartist/Home.kt
@@ -10,6 +10,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
+import android.view.WindowManager
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -30,39 +32,61 @@ import java.io.InputStream
 import java.lang.Exception
 
 class Home : Fragment() {
+    // Создаем новый экземпляр CoroutineScope, который будет использоваться для запуска корутин в основном потоке.
     val coroutineScope = CoroutineScope(Dispatchers.Main + Job())
+
+    // Создаем ссылку на объект SupaBaseClient, который предоставляет доступ к базе данных Supabase.
     val supabase = SupaBaseClient().ClientSB
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_home, container, false)
-        val recyclerView: RecyclerView = view.findViewById(R.id.recycler_popular_artist)
-        recyclerView.layoutManager= LinearLayoutManager(context)
 
+        // Инициализируем корневой вид фрагмента, используя layout файл fragment_home.
+        val view = inflater.inflate(R.layout.fragment_home, container, false)
+
+        // Находим RecyclerView в разметке и устанавливаем горизонтальный макетщик.
+        val recyclerView: RecyclerView = view.findViewById(R.id.recycler_popular_artist)
+        recyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+
+        // Запускаем корутину для получения данных из базы данных Supabase.
         lifecycleScope.launch {
             try {
+                // Создаем пустой список для хранения данных артистов.
                 val artists = mutableListOf<DataArtist>()
+
+                // Получаем данные артистов из базы данных.
                 val artist = supabase.from("Исполнители").select(columns = Columns.list("id, Имя, Аватар")).data
+
+                // Преобразуем полученные данные в JSONArray.
                 val jsonArray = JSONArray(artist)
 
+                // Перебираем элементы JSONArray и добавляем их в список artists.
                 for (i in 0 until jsonArray.length()) {
                     val jsonObject = jsonArray.getJSONObject(i)
+                    // Получаем значения ID, имени и аватара артиста из JSONObject.
+                    val id = jsonObject.getInt("id")
                     val name = jsonObject.getString("Имя")
                     val ava = jsonObject.getString("Аватар")
-                    val dataArtist = DataArtist(Имя = name, Аватар = ava)
+                    // Создаем экземпляр DataArtist и добавляем его в список artists.
+                    val dataArtist = DataArtist(id = id, Имя = name, Аватар = ava)
                     Log.e("DAndahsda", dataArtist.toString())
                     artists.add(dataArtist)
                 }
+                // Создаем адаптер для списка artists и устанавливаем его в RecyclerView.
                 val adapter = AdapterArtists(artists, coroutineScope)
                 recyclerView.adapter = adapter
-            }catch (e: Exception){
+            } catch (e: Exception) {
+                // Логируем любую возникшую ошибку.
                 Log.e("!!!!!!!", e.toString())
             }
         }
+        // Возвращаем корневой вид фрагмента.
         return view
     }
 }
+
 
 
